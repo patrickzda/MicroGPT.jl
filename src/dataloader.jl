b07104638c83bcf2257824ebb41ea22020d19227
Tobias303 <tobiaskirschner10@gmail.com>
2026-06-16 14:26:09 +0200

    Dataloader refactoring

---
+++ src/dataloader.jl
@@ -0,0 +1,19 @@
+
+
+
+
+const NAMES_URL = "https://raw.githubusercontent.com/karpathy/makemore/refs/heads/master/names.txt"
+
+function load_data(path::String="input.txt"; shuffle::Bool=true, rng=Random.GLOBAL_RNG)::Vector{String}
+    if !isfile(path)
+        #		@info "Downloading names dataset to $path ..."
+        Downloads.download(NAMES_URL, path)
+    end
+    docs = [strip(l) for l in eachline(path) if !isempty(strip(l))]
+    #	Filter auf a-z?
+    #	docs = filter(doc -> all(c -> 'a' <= c <= 'z', doc), docs)  # nur a-z
+    shuffle && Random.shuffle!(rng, docs)
+    #	@info "num docs: $(length(docs))"
+    return docs
+end
+
